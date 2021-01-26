574b4d6e86cf12567b0b3d9b98ae574ee3e24bf5
Pwg21 <kcaskade@gmail.com>
2021-01-26 15:17:16 -0700

    Finished v3, very annoying and challenging, created functions that randomized wcharacters and character order for password as well as added function that dispables submit button until criteria is choosen

--- Develop/script-v2.js
+++ Develop/script-v2.js
@@ -1,141 +1,129 @@
 // ***** in this js version, form 1 is skipped
 
 // Assignment Code
-// starter page w/ button 
+// starter page w/ button
 const generateBtn = document.querySelector("#generate");
 
 // form2
-// form2-buttons 
+// form2-buttons
 const form2Btn = document.querySelector("#form2-btn");
 
 // fotm2-range bar and value with function to link both
-const rangeBar = document.querySelector('#rangebar')
-const rangeAmount = document.querySelector('#rangeamount')
+const rangeBar = document.querySelector("#rangebar");
+const rangeAmount = document.querySelector("#rangeamount");
 
-rangeBar.addEventListener('input', syncBartoAmount)
-rangeAmount.addEventListener('input', syncBartoAmount)
-
+rangeBar.addEventListener("input", syncBartoAmount);
+rangeAmount.addEventListener("input", syncBartoAmount);
+function syncBartoAmount(e) {
+  const value = e.target.value;
+  rangeBar.value = value;
+  rangeAmount.value = value;
+}
 
 // form3
 // form-3 buttons
 const form3Btn = document.querySelector("#form3-btn");
 
 // form-3 check boxes
-const upperCase = document.querySelector('#UL')
-const lowerCase = document.querySelector('#LL')
-const numbers = document.querySelector('#num')
-const special = document.querySelector('#special')
-
+const upperCase = document.querySelector("#UL");
+const lowerCase = document.querySelector("#LL");
+const numbers = document.querySelector("#num");
+const special = document.querySelector("#special");
 
 // encompasses all criteria for all forms
-const criteria = document.querySelector('#criteria')
+const criteria = document.querySelector("#criteria");
 
 // vairiable for password
 const passwordText = document.querySelector("#password");
 
-
 // arrays of special symbols
 
-const UPPERCASE_CHAR_CODES = makeCharArray(65, 90)
-const LOWERCASE_CHAR_CODES = makeCharArray(97, 122)
-const NUMBER_CHAR_CODES = makeCharArray(48, 57)
-const SYMBOL_CHAR_CODES = makeCharArray(33, 47).concat(
-  makeCharArray(58, 64)
-).concat(
-  makeCharArray(91, 96)
-).concat(
-  makeCharArray(123, 126)
-)
-
-
+const UPPERCASE_CHAR_CODES = makeCharArray(65, 90);
+const LOWERCASE_CHAR_CODES = makeCharArray(97, 122);
+const NUMBER_CHAR_CODES = makeCharArray(48, 57);
+const SYMBOL_CHAR_CODES = makeCharArray(33, 47)
+  .concat(makeCharArray(58, 64))
+  .concat(makeCharArray(91, 96))
+  .concat(makeCharArray(123, 126));
 
 //  values of length and character depededing on what the user chooses
-criteria.addEventListener('submit', e => {
-  e.preventDefault()
+criteria.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  const characterAmount = rangeAmount.value
-  const upperCaseChecked = upperCase.checked
-  const lowerCaseChecked = lowerCase.checked
-  const numbersChecked = numbers.checked
-  const specialChecked = special.checked
-  
+  const characterAmount = rangeAmount.value;
+  const upperCaseChecked = upperCase.checked;
+  const lowerCaseChecked = lowerCase.checked;
+  const numbersChecked = numbers.checked;
+  const specialChecked = special.checked;
+
   // // document.querySelector("#form3").style.display = "none";
   // // document.querySelector(".wrapper").style.display = "block";
-  
-  const finalpassword = generatePassword(characterAmount, upperCaseChecked, lowerCaseChecked, numbersChecked, specialChecked)
-  passwordText.innerHTML = finalpassword
-})
 
-
+  const finalpassword = generatePassword(
+    characterAmount,
+    upperCaseChecked,
+    lowerCaseChecked,
+    numbersChecked,
+    specialChecked
+  );
+  passwordText.innerHTML = finalpassword;
+});
 
 // generate password function
-function generatePassword(characterAmount, upperCaseChecked, lowerCaseChecked, numbersChecked, specialChecked) {
-  let availableChars = []
+function generatePassword(
+  characterAmount,
+  upperCaseChecked,
+  lowerCaseChecked,
+  numbersChecked,
+  specialChecked
+) {
+  let availableChars = [];
 
-  
-  
-  if (upperCaseChecked) availableChars = availableChars.concat(UPPERCASE_CHAR_CODES)
-  if (lowerCaseChecked) availableChars = availableChars.concat(LOWERCASE_CHAR_CODES)
-  if (numbersChecked) availableChars = availableChars.concat(NUMBER_CHAR_CODES)
-  if (specialChecked) availableChars = availableChars.concat(SYMBOL_CHAR_CODES )
+  if (upperCaseChecked)
+    availableChars = availableChars.concat(UPPERCASE_CHAR_CODES);
+  if (lowerCaseChecked)
+    availableChars = availableChars.concat(LOWERCASE_CHAR_CODES);
+  if (numbersChecked) availableChars = availableChars.concat(NUMBER_CHAR_CODES);
+  if (specialChecked) availableChars = availableChars.concat(SYMBOL_CHAR_CODES);
 
-  const passwordChars = []
+  const passwordChars = [];
   for (let i = 0; i < rangeAmount; i++) {
-    const singlePasswordChar = characterAmount[Math.floor(Math.random() * availableChars.length)]
-    passwordChars.push(String.fromCharCode(singlePasswordChar))
+    const singlePasswordChar =
+      characterAmount[Math.floor(Math.random() * availableChars.length)];
+    passwordChars.push(String.fromCharCode(singlePasswordChar));
   }
-  return passwordChars.join('')
+  return passwordChars.join("");
 }
-
 
 // functions to generate different symbols based of char codes
 function makeCharArra3y(low, high) {
-  const array = []
-  for (let i = low; i =< high; i++) {
-    array.push(i)
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
   }
-  return array
+  return array;
 }
-
 
 // syncs rangebar to value
 function syncBartoAmount(e) {
-  const value = e.target.value
-  rangeBar.value = value
-  rangeAmount.value = value
+  const value = e.target.value;
+  rangeBar.value = value;
+  rangeAmount.value = value;
 }
-
-
-
-
-
-
-
-
 
 // pop-up windows
 generateBtn.addEventListener("click", function () {
   document.querySelector(".wrapper").style.display = "none";
   document.querySelector(".pop-up").style.display = "flex3";
   document.querySelector("#form2").style.display = "flex";
-})
+});
 
 form2Btn.addEventListener("click", function () {
   document.querySelector("#form2").style.display = "none";
   document.querySelector("#form3").style.display = "flex";
- })
+});
 
-form3Btn.addEventListener("click", function(){
-  document.querySelector("#form3").style.display="none";
-  document.querySelector(".wrapper").style.display="block"; 
-  })
-
-
-
-
-
-
-
-
-
-
+form3Btn.addEventListener("click", function () {
+  document.querySelector("#form3").style.display = "none";
+  document.querySelector(".wrapper").style.display = "block";
+});
--- Develop/script-v3.js
+++ Develop/script-v3.js
@@ -1,9 +1,164 @@
 // seperate pages
-var startPage = document.querySelector("#start-button");
-var rangeBar = document.querySelector("#rangebar-page");
-var Checkbox = document.querySelector("#check-box-page");
+const startPage = document.querySelector("#start-page");
+const rangebarPage = document.querySelector("#rangebar-page");
+const CheckboxPage = document.querySelector("#check-box-page");
 
 // buttons
-var startBtn = document.querySelector("#start-button");
-var continueBtn = document.querySelector("#continue-button");
-var submitBtn = document.querySelector("#submit-btn");
+const startBtn = document.querySelector("#start-btn");
+const continueBtn = document.querySelector("#continue-btn");
+const submitBtn = document.querySelector("#submit-btn");
+
+// rangebar vairiables
+var rangeBar = document.querySelector("#range-bar");
+var rangeAmount = document.querySelector("#range-amount");
+
+rangeBar.addEventListener("input", syncBar);
+rangeAmount.addEventListener("input", syncBar);
+
+// checkbox vairiables
+var upperCaseSelect = document.querySelector("#uppercase");
+var lowerCaseSelect = document.querySelector("#lowercase");
+var numbersSelect = document.querySelector("#numbers");
+var specialSelect = document.querySelector("#specials");
+
+// write arrea where new password goes
+const writeArea = document.querySelector("#password");
+
+var lowerCaseChar = makeArray(97, 122);
+var upperCaseChar = makeArray(65, 90);
+var numberChar = makeArray(48, 57);
+var specialChar = makeArray(33, 47)
+  .concat(makeArray(58, 64))
+  .concat(makeArray(91, 96))
+  .concat(makeArray(123, 125));
+
+// create password function
+function makePassword(
+  characterAmount,
+  lowerCaseSelect,
+  upperCaseSelect,
+  numbersSelect,
+  specialSelect
+) {
+  var possiblePasswordCharinArray = [];
+  var passwordCharsinArray = [];
+
+  if (lowerCaseSelect) {
+    passwordCharsinArray.push(
+      String.fromCharCode(getRandFromArr(lowerCaseChar))
+    );
+    possiblePasswordCharinArray = possiblePasswordCharinArray.concat(
+      lowerCaseChar
+    );
+  }
+
+  if (upperCaseSelect) {
+    passwordCharsinArray.push(
+      String.fromCharCode(getRandFromArr(upperCaseChar))
+    );
+    possiblePasswordCharinArray = possiblePasswordCharinArray.concat(
+      upperCaseChar
+    );
+  }
+
+  if (numbersSelect) {
+    passwordCharsinArray.push(String.fromCharCode(getRandFromArr(numberChar)));
+    possiblePasswordCharinArray = possiblePasswordCharinArray.concat(
+      numberChar
+    );
+  }
+
+  if (specialSelect) {
+    passwordCharsinArray.push(String.fromCharCode(getRandFromArr(specialChar)));
+    possiblePasswordCharinArray = possiblePasswordCharinArray.concat(
+      specialChar
+    );
+  }
+
+  for (let i = passwordCharsinArray.length; i < characterAmount; i++) {
+    let characterCode = getRandFromArr(possiblePasswordCharinArray);
+
+    passwordCharsinArray.push(String.fromCharCode(characterCode));
+  }
+  return shuffle(passwordCharsinArray).join("");
+}
+
+// function that shuffles the indexes password characters
+function shuffle(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+
+    var tempOne = arr[i];
+    var tempTwo = arr[randIndex];
+
+    arr[i] = tempTwo;
+    arr[randIndex] = tempOne;
+  }
+
+  return arr;
+}
+
+// function that chooses random characters to possibly use
+function getRandFromArr(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// function that creates generates array with low and high value
+function makeArray(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+// synchs value of range bar to number value
+function syncBar(event) {
+  var value = event.target.value;
+  rangeBar.value = value;
+  rangeAmount.value = value;
+}
+
+// function that moves on to password length page
+startBtn.addEventListener("click", function () {
+  startPage.style.display = "none";
+  rangebarPage.style.display = "block";
+});
+
+// function that moves on to password characters
+continueBtn.addEventListener("click", function () {
+  CheckboxPage.style.display = "block";
+  rangebarPage.style.display = "none";
+});
+
+// function that submits everything to creat password
+submitBtn.addEventListener("click", function () {
+  var characterAmount = rangeAmount.value;
+  var lowerCaseSelected = lowerCaseSelect.checked;
+  var upperCaseSelected = upperCaseSelect.checked;
+  var numbersSelected = numbersSelect.checked;
+  var specialSelected = specialSelect.checked;
+
+  if (
+    !(
+      lowerCaseSelected ||
+      upperCaseSelected ||
+      numbersSelected ||
+      specialSelected
+    )
+  ) {
+    return;
+  }
+
+  var password = makePassword(
+    characterAmount,
+    lowerCaseSelected,
+    upperCaseSelected,
+    numbersSelected,
+    specialSelected
+  );
+
+  writeArea.innerText = password;
+  startPage.style.display = "block";
+  CheckboxPage.style.display = "none";
+});
